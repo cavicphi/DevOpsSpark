237b3a41befb56c1ea3e625202adfe1f8660fe37
cavicphi <cavicphi@students.zhaw.ch>
2022-05-19 16:41:19 +0200

    Test Sparks

--- app/src/main/java/DevOpsSpark/App.java
+++ app/src/main/java/DevOpsSpark/App.java
@@ -12,7 +12,7 @@ public class App {
 
     public static void main(String[] args) {
         Spark.get("/welcome", (req, res) -> {
-            return "Hallo DevOps";
+            return "Hallo Philip";
         });
     }
 }
